b36496bbee57de260f05d4c358b72ba5a14f0870
amitasulin <amitasulin7@gmail.com>
2024-02-12 02:32:38 +0200

    1st part completed - backend

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+import authRoutes from './routes/auth.routes.js';
+import messageRoutes from './routes/message.routes.js';
+import userRoutes from './routes/user.routes.js';
+
+import connectToMongoDB from './db/connectToMongoDB.js';
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+dotenv.config();
+
+app.use(express.json()); 
+app.use(cookieParser());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
+app.use('/api/users', userRoutes);
+
+
+
+// app.get('/', (req,res) => {
+//     res.send("Hello World")
+// });
+
+
+
+app.listen(PORT, () => {
+    connectToMongoDB()
+    console.log(`Server running on port ${PORT}`);
+})
+
