fc46a95ec33f95f222a508f24e81dfd54dd79d38
amitasulin <amitasulin7@gmail.com>
2024-03-10 20:02:58 +0200

    sabaab

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,20 +1,25 @@
 import React from "react";
+import useGetMessages from "../../hooks/useGetMessages";
+import MessageSkeleton from "../skeletons/MessageSkeleton";
 import Message from "./Message";
 
 const Messages = () => {
+  const { messages, loading } = useGetMessages();
+  console.log("messages:", messages);
   return (
     <div className="px-4 flex-1 overflow-auto">
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
+      {!loading && messages.length > 0
+        ? messages.map((message) => (
+            <Message key={message.message._id} message={message.message} />
+          ))
+        : null}
+
+      {loading
+        ? [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)
+        : null}
+      {!loading && messages.length === 0 ? (
+        <p className="text-center">Send a message to start the conversation</p>
+      ) : null}
     </div>
   );
 };
